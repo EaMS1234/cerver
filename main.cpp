17f733c53557c9cb2e9f20688f6339ba2bccecb1
Erick Augusto <contato@eams.dev.br>
2026-03-11 14:53:30 -0300

    Parses the first line of HTTP request into a struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <cstring>
 #include <iostream>
 #include <thread>
+#include <queue>
+#include <string>
 
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -8,7 +10,33 @@
 #include <unistd.h>
 
 
+// TODO: Add other methods
+typedef enum
+{
+  GET = 0
+}
+http_method;
+
+// TODO: Add support to version 2
+typedef enum
+{
+  VER_11 = 1
+}
+http_version;
+
+
+// TODO: Parse the headers as well
+typedef struct
+{
+  http_method method;
+  http_version version;
+  char * path;  
+}
+request;
+
+
 void message(int socket);
+int parserequest(request * req, char * message);
 
 
 int main()
@@ -25,7 +53,7 @@ int main()
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == 0)
   {
-    std::cout << "Could not create socket\n";
+    std::cout << "Could not create socket" << std::endl;
     return sockfd;
   }
 
@@ -41,7 +69,7 @@ int main()
   err = bind(sockfd, (struct sockaddr *)&address, addr_len);
   if (err < 0)
   {
-    std::cout << "Could not bind socket to address\n";
+    std::cout << "Could not bind socket to address" << std::endl;
     return err;
   }
 
@@ -49,7 +77,7 @@ int main()
   err = listen(sockfd, 5);
   if (err < 0)
   {
-    std::cout << "Could not listen\n";
+    std::cout << "Could not listen" << std::endl;
     return err;
   }
 
@@ -59,13 +87,14 @@ int main()
     temp = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addr_len);
     if (temp < 0)
     {
-      std::cout << "Could not accept connection\n";
+      std::cout << "Could not accept connection" << std::endl;
       close(temp);
     }
-
-    // Creates a new thread for fetching messages asynchronously
-    std::thread msg(message, temp);
-    msg.detach();
+    else
+    {
+      std::thread msg(message, temp);
+      msg.detach();
+    }
   }
   
   close(sockfd);
@@ -79,6 +108,9 @@ void message(int socket)
   char buffer[1024] = {0};         // buffer with the received message
   msghdr msg;                      // the message itself
   iovec vec;                       // io vector for the message
+  request req;
+  
+  int err;
 
   // Sets up the IO vector and the message
   vec.iov_base = &buffer;
@@ -86,22 +118,97 @@ void message(int socket)
   msg.msg_iov = &vec;
   msg.msg_iovlen = 1;
 
-  // Keeps looking for messages
-  while (1)
+  while(1)
   {
     // clears the buffer
     memset(buffer, 0, 1024);
 
     if (recvmsg(socket, &msg, 0) > 0)
     {
-      std::cout << buffer;
+      err = parserequest(&req, buffer);
+      if (err != 0)
+      {
+        send(socket, "HTTP/1.1 400", 13, 0);
+        break;
+      }
+      else
+      {
+        send(socket, "HTTP/1.1 200\nServer: Cerver\nConnection: close\nContent-Length: 21\nContent-Type: text/html\n\n<p>Hello, World!</p>\n", 112, 0);
+      }
     }
     else
     {
-      std::cout << "Connection closed\n";
-      close(socket);
+      std::cout << "Connection closed" << std::endl;
       break;
     }
   }
+
+  close(socket);
+}
+
+
+int parserequest(request * req, char * message)
+{
+  char buffer[1024] = {0};
+  std::queue<std::string> data;  // Three elements (front to back): Method, Path, Version
+                                
+  std::cout << message << std::endl;
+
+  int i = 0;
+  int j = 0;
+  while (1)
+  {
+    if (message[i] == ' ' || message[i] == '\n' || message[i] == '\0')
+    {
+      data.emplace(buffer);
+      memset(buffer, 0, 1024);
+
+      j = i + 1;
+
+      if (message[i] != ' ')
+      {
+        break;
+      }
+    }
+    else
+    {
+      buffer[i - j] = message[i];
+    }
+
+    i++;
+  }
+
+  // Failed to grab the method, version and path somewhere along the way
+  if (data.size() != 3)
+  {
+    return 1;
+  }
+
+  if (strcmp(data.front().c_str(), "GET") == 0)
+  {
+    req->method = GET;
+    data.pop();
+  }
+  else
+  {
+    return -1;
+  }
+
+  // HTTP Path
+  req->path = (char *)data.front().c_str();
+  data.pop();
+ 
+  // HTTP Version
+  if (strcmp(data.front().c_str(), "HTTP/1.1"))
+  {
+    req->version = VER_11;
+    data.pop();
+  }
+  else
+  {
+    return -1;
+  }
+
+  return 0;
 }
 
